94d2b5f78b7c5ac842c0a3d1084917a5392b9142
Jae Yong Kim <kim900180@gmail.com>
2018-07-11 05:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     
   }
-  return "Currently serving " + name + "."
-  katzDeliLine.shift()
+  
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
