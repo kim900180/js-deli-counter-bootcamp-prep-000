8a9b07734daa9c705e1310185237362622443477
Jae Yong Kim <kim900180@gmail.com>
2018-07-11 05:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
+    var message = ""
     for (var i = 0; i < katzDeliLine.length; i++) {
-      var message = ""
-      message = message + i + 1 + ". " katzDeliLine[i]
+      message = message + math.(i + 1) + ". " katzDeliLine[i]
     }
+    return "The line is currently: " + message
+  } else {
+    return "The line is currently empty."
   }
 }
